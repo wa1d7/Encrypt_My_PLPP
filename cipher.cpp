557b84054e19e925cfaf1ae6b2a4cb4e1ee5c354
wa1d7 <ignat.chybuk@gmail.com>
2026-06-17 01:54:56 +0300

    Initial commit

---
+++ cipher.cpp
@@ -0,0 +1,25 @@
+﻿//
+// Created by ignat on 17.06.2026.
+//
+
+#include "cipher.h"
+CaesarCipher::CaesarCipher(int key) {
+    key_ = key;
+}
+std::string CaesarCipher::encrypt(const std::string& text) {
+    return text;
+}
+std::string CaesarCipher::decrypt(const std::string& text) {
+    return text;
+}
+
+VigenereCipher::VigenereCipher(const std::string& key) {
+    key_ = key;
+}
+std::string VigenereCipher::encrypt(const std::string& text) {
+    return text;
+}
+
+std::string VigenereCipher::decrypt(const std::string& text){
+    return text;
+};
---
+++ cipher.h
@@ -0,0 +1,33 @@
+﻿//
+// Created by ignat on 17.06.2026.
+//
+
+#ifndef CIPHER_H
+#define CIPHER_H
+
+#include <string>
+
+class Cipher {
+public:
+    virtual std::string encrypt(const std::string& text) = 0;
+    virtual std::string decrypt(const std::string& text) = 0;
+    virtual ~Cipher() = default;
+};
+
+class CaesarCipher : public Cipher {
+    int key_;
+public:
+    CaesarCipher(int key);
+    std::string encrypt(const std::string& text) override;
+    std::string decrypt(const std::string& text) override;
+};
+
+class VigenereCipher : public Cipher {
+    std::string key_;
+public:
+    VigenereCipher(const std::string& key);
+    std::string encrypt(const std::string& text) override;
+    std::string decrypt(const std::string& text) override;
+};
+
+#endif // CIPHER_H
