0162462ac14fd15a1e3dd6be9843823e44a45b45
wa1d7 <ignat.chybuk@gmail.com>
2026-06-27 15:43:25 +0300

    add abstract Line class and basic TextLine implementation

---
+++ line.cpp
@@ -0,0 +1,51 @@
+﻿//
+// Created by ignat on 13.07.2026.
+//
+
+#include "line.h"
+
+using namespace std;
+
+TextLine::TextLine(const string& t) : text(t) {}
+
+void TextLine::print() const {
+    cout << "Text: " << text << endl;
+}
+
+unique_ptr<Line> TextLine::clone() const {
+    return make_unique<TextLine>(text);
+}
+
+int TextLine::length() const { return text.length(); }
+
+void TextLine::append(const string& str) { text += str; }
+
+void TextLine::insert(int char_idx, const string& str) {
+    if (char_idx >= 0 && char_idx <= text.length()) {
+        text.insert(char_idx, str);
+    }
+}
+
+void TextLine::remove(int char_idx, int count) {
+    if (char_idx >= 0 && char_idx <= text.length()) {
+        text.erase(char_idx, count);
+    }
+}
+
+void TextLine::insertReplace(int char_idx, const string& str) {
+    if (char_idx >= 0 && char_idx <= text.length()) {
+        text.replace(char_idx, str.length(), str);
+    }
+}
+
+string TextLine::extract(int char_idx, int count) const {
+    if (char_idx >= 0 && char_idx < text.length()) {
+        return text.substr(char_idx, count);
+    }
+    return "";
+}
+
+bool TextLine::contains(const string& query, size_t& pos) const {
+    pos = text.find(query, pos);
+    return pos != string::npos;
+}
---
+++ line.h
@@ -0,0 +1,41 @@
+﻿#ifndef LINE_H
+#define LINE_H
+
+#include <string>
+#include <iostream>
+#include <memory>
+
+class Line {
+public:
+    virtual ~Line() = default;
+
+    virtual void print() const = 0;
+
+    virtual std::unique_ptr<Line> clone() const = 0;
+
+    virtual int length() const { return 0; }
+    virtual void append(const std::string& str) {}
+    virtual void insert(int char_idx, const std::string& str) {}
+    virtual void remove(int char_idx, int count) {}
+    virtual void insertReplace(int char_idx, const std::string& str) {}
+    virtual std::string extract(int char_idx, int count) const { return ""; }
+    virtual bool contains(const std::string& query, size_t& pos) const { return false; }
+};
+
+class TextLine : public Line {
+    std::string text;
+public:
+    explicit TextLine(const std::string& t);
+    void print() const override;
+    std::unique_ptr<Line> clone() const override;
+
+    int length() const override;
+    void append(const std::string& str) override;
+    void insert(int char_idx, const std::string& str) override;
+    void remove(int char_idx, int count) override;
+    void insertReplace(int char_idx, const std::string& str) override;
+    std::string extract(int char_idx, int count) const override;
+    bool contains(const std::string& query, size_t& pos) const override;
+};
+
+#endif
